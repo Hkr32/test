38fb592650b9acc7100b1615428d0fe52e89af09
IgorArt <artushkin.igor@gmail.com>
2023-06-12 22:05:07 +0300

    added styles

--- resources/views/app.php
+++ resources/views/app.php
@@ -1,13 +1,14 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8" />
     <title><?= $this->safe($config['name']); ?></title>
 
     <style>
         body { padding: 16px 30px; }
-        header { margin-bottom: 3rem; font-weight: bold; }
-        button { padding: 3px 10px; background: #63b614; border: 1px solid #48850e; border-radius: 3px; color: #fff; }
+        header { margin-bottom: 3rem; font-size: 1.3rem; font-weight: bold; }
+        button { padding: 3px 10px; background: #63b614; border: 1px solid #48850e; border-radius: 3px; color: #fff; cursor: pointer; }
+        p { margin-bottom: 1rem; }
     </style>
 </head>
 <body>
