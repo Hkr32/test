bf4b9cd4dfad0f1fb2d5bd5b6dfff48af0a2e1f6
IgorArt <artushkin.igor@gmail.com>
2023-06-14 16:42:39 +0300

    throw error if user has already promo code

--- app/Queries/PromoCodeQuery.php
+++ app/Queries/PromoCodeQuery.php
@@ -2,6 +2,8 @@
 
 namespace App\Queries;
 
+use App\Exceptions\AppException;
+
 final class PromoCodeQuery extends BaseQuery
 {
     public function getCodeByUser(int $userId): ?string
@@ -15,6 +17,10 @@ final class PromoCodeQuery extends BaseQuery
 
     public function takeCodeForUser(int $userId): bool
     {
+        if ($this->hasUser($userId)) {
+            throw new AppException('The user already has a promo code.');
+        }
+
         $query = 'UPDATE `promocodes` SET `user_id` = ?, `busy_at` = ? WHERE `user_id` IS NULL LIMIT 1';
 
         $now = date('Y-m-d H:i:s');
@@ -31,5 +37,14 @@ final class PromoCodeQuery extends BaseQuery
         $result = $this->db->query($query)->fetch();
 
         return $result['user_id'] ?? null;
+    }
+
+    public function hasUser(int $userId): bool
+    {
+        $query = 'SELECT EXISTS(SELECT * FROM `promocodes` WHERE `user_id` = ?) as `has`';
+
+        $result = $this->db->query($query, [$userId])->fetch();
+
+        return (bool) $result['has'];
     }
 }
